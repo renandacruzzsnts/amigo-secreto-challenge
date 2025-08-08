5cdcb43017ba6eb8fc68c466d2f54a6a38f4515e
= <cleomardestak@gmail.com>
2025-08-08 11:15:40 -0300

    Implementa função para adicionar amigos à lista

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,22 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeAmigos  = [];
+
+function adicionarAmigo(){
+    let entrada = document.getElementById("amigo");
+    let nome = entrada.value;
+
+    if(nome == ""){
+        alert("Por favor, digite um nome.");
+        return;
+    }
+
+    listaDeAmigos.push(nome);
+    
+    let lista = document.getElementById("listaDeAmigos");
+    let item = document.createElement("li");
+    item.innerText = nome;
+    lista.appendChild(item)
+
+
+    entrada.value = "";
+}
+
